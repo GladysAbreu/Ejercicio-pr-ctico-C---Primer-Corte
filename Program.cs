1e7d5bcf4970a57c6f8a1471e8576aef5304a3b1
GladysAbreu <92760808+GladysAbreu@users.noreply.github.com>
2021-10-18 23:41:03 -0400

    Estudiante: Gladysmar Abreu - Programación I

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Proyectos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Asignacion de valores de las variables
+            int a = 8;
+            int b = 3; 
+            int c = 6;
+
+           Console.WriteLine("A continuacion, se muestran diversas combinaciones numericas, las cuales son:");
+
+           // Combinacion: aacb
+           Console.WriteLine("Primera combinacion: " + a + a + c + b);
+
+           // Combinacion: bca
+           Console.WriteLine($"Segunda combinacion: {b}{c}{a}");
+
+           // Combinacion: bcbc
+           Console.WriteLine("Tercera combinacion: {0}{1}{2}{3}", b,c,b,c);
+              
+        }
+    }
+}
